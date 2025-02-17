686de4fe0349ca0447f27761a32222e480056e25
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-02-17 09:39:26 +0100

    Gradient

---
+++ client/src/components/HomeComp/SearchGrad.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+
+function SearchGrad() {
+  return (
+    <div>
+        <div className="purple-grad bg-gradient-to-r from-theme to-[#130121] py-20 text-white flex flex-col items-center rounded-2xl justify-center">
+            <p className=' text-4xl font-medium text-center'>Over 10,000+ Jobs to Apply</p>
+            <p className=' text-center text-sm mb-14 mt-4'>Your Next Big career Move Starts Right Here <br></br>Explore The Best Job Opportunities and Take the First Step Toward Your Future!</p>
+            <div className="search-bar">
+            <div className="relative w-full max-w-[599px] text-text">
+                    <CiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+
+                    <input
+                        type="text"
+                        className=" w-[599px] pl-10 pr-4 py-4 bg-white rounded focus:outline-none"
+                        placeholder=" Search for Jobs"
+                    />
+         
+         <CiLocationOn className="absolute right-[18rem] top-1/2 -translate-y-1/2 text-gray-500" />
+
+         <p className=' absolute right-[13rem] top-1/2 -translate-y-1/2'>Location</p>
+
+         <button className=' absolute right-3 rounded top-1/2 -translate-y-1/2 bg-theme py-3 px-8 text-white hover:bg-theme-hover cursor-pointer'>Search</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchGrad
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import SearchGrad from '../components/HomeComp/SearchGrad'
 
 const Home = () => {
   return (
-    <div className=' mx-16'>
+    <div className='mx-5 md:mx-16'>
       <Navbar/>
+      <SearchGrad/>
     </div>
   )
 }
