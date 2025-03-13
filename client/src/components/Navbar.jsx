5dd9346200908a11646aba10a19090496b3ed926
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-03-13 19:09:00 +0100

    nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Assets } from '../assets/Assets'
+import {useNavigate} from 'react-router-dom'
 
 function Navbar() {
+
+  const navigate = useNavigate()
   return (
     <div className=' flex items-center justify-between py-6 '>
         <div className="Logo flex items-center">
-        <img src={Assets.logo} alt="JobNest Logo" className=' w-16'/>
+          <div onClick={navigate('../JobDescription')}>
+        <img src={Assets.logo} alt="JobNest Logo" className=' md:w-16'/>
+        </div>
         <a href='' className=' text-2xl font-bold'>JobNest</a>
         </div>
 
