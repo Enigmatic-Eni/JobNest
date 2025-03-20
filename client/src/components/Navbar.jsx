e7b1d117a2ed58341ffded0a886a54cf70e09817
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-03-20 18:02:31 +0100

    added form tabs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,51 +1,58 @@
-import React from 'react'
-import { Assets } from '../assets/Assets'
-import {useNavigate} from 'react-router-dom'
-import { Button } from './ui/button'
+import React from "react";
+import { Assets } from "../assets/Assets";
+// import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Navbar() {
-
   // const navigate = useNavigate()
   return (
-    <div className=' flex items-center justify-between py-6 '>
-        <div className="Logo flex items-center">
-          <div >
-        <img src={Assets.logo} alt="JobNest Logo" className=' md:w-16'/>
+    <div className=" flex items-center justify-between py-6 ">
+      <div className="Logo flex items-center">
+        <div>
+          <img src={Assets.logo} alt="JobNest Logo" className=" md:w-16" />
         </div>
-        <a href='' className=' text-2xl font-bold'>JobNest</a>
-        </div>
+        <a href="" className=" text-2xl font-bold">
+          JobNest
+        </a>
+      </div>
 
-        <div className="login-button space-x-3">
-          <div>
+      <div className="login-button flex items-center space-x-3">
+        <div>
           <Dialog>
-  <DialogTrigger><a href="" className=' hover:underline'>Recruiter Login</a></DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
-          </div>
-            <a href="" className=' hover:underline'>Recruiter Login</a>
-            <Button variant="rounded" className=" px-7 py-2 text-base">Register</Button>
+            <DialogTrigger>
+              <button className="hover:underline">Login</button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogDescription>
+                <Tabs defaultValue="account" className="w-[400px]">
+                  <TabsList>
+                    <TabsTrigger value="account">Recruiter</TabsTrigger>
+                    <TabsTrigger value="password">Job Seeker</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="account">
+                    Make changes to your account here.
+                  </TabsContent>
+                  <TabsContent value="password">
+                    Change your password here.
+                  </TabsContent>
+                </Tabs>
+              </DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
-
-
+        <Button variant="rounded" className=" px-7 py-2 text-base">
+          Register
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ client/src/components/SignIn/RecruiterSignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecruiterSignIn() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default RecruiterSignIn
