c6de441cb19c0b44432632ba2b7973cfa1fbfc9e
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-08-03 23:48:41 +0100

    dependencies

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const connectToDB = require("./database/db");
-const authRoutes = require("./routes/auth-routes");
+
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors({ 
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173',
+    credentials: true 
   
  }))
 
@@ -20,10 +21,11 @@ connectToDB();
 // Middleware
 app.use(express.json());
 
-// Routes
-app.use("/auth", authRoutes);
 
 
+app.get("/test-cors", (req, res) => {
+  res.json({ message: "CORS is working! Lmao" });
+});
 
 
 app.listen(PORT, () => {
